499d443dae6433addad5a8ded1efaa88dd491c7e
eyup <eyuptozcu@gmail.com>
2025-11-08 01:27:12 +0000

    Refactor test configuration to use Testcontainers for PostgreSQL and add minimal integration test

--- data-broker-service/src/test/java/com/example/broker/config/BaseTestConfiguration.java
+++ data-broker-service/src/test/java/com/example/broker/config/BaseTestConfiguration.java
@@ -1,30 +1,13 @@
 package com.example.broker.config;
 
+import com.example.broker.ContainersConfig;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@Testcontainers
+@Import({ContainersConfig.class, SecurityTestConfig.class})
 public abstract class BaseTestConfiguration {
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
-            .withDatabaseName("testdb")
-            .withUsername("test")
-            .withPassword("test")
-            .withReuse(true);
-
-    @DynamicPropertySource
-    static void databaseProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-        registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
-    }
+    // This class serves as a base for all tests, importing common configurations
 }
