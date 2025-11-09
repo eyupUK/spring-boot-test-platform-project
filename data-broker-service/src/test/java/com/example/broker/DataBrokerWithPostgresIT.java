1f325d2d33e3d5382a43cecacbc78884baa9151f
eyup <eyuptozcu@gmail.com>
2025-11-09 17:09:34 +0000

    Integrate Testcontainers for PostgreSQL in integration tests and exclude DataBrokerWithPostgresIT from Maven Failsafe plugin

--- data-broker-service/src/test/java/com/example/broker/DataBrokerWithPostgresIT.java
+++ data-broker-service/src/test/java/com/example/broker/DataBrokerWithPostgresIT.java
@@ -5,15 +5,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 public class DataBrokerWithPostgresIT {
 
+  @Container
   static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
 
   @DynamicPropertySource
   static void props(DynamicPropertyRegistry r) {
-    postgres.start();
     r.add("spring.datasource.url", postgres::getJdbcUrl);
     r.add("spring.datasource.username", postgres::getUsername);
     r.add("spring.datasource.password", postgres::getPassword);
